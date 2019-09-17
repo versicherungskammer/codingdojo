96800bf24f98f9d2879a7b9e7dd7472573451d0d
Herr Xia <bai@Bais-MBP.fritz.box>
2019-09-17 23:25:21 +0200

    Life is short, I use Python.

--- BaiPython/life_is_short.py
+++ BaiPython/life_is_short.py
@@ -1,15 +1,10 @@
 guess_dict: dict = {'rock': 0, 'spock': 1, 'paper': 2, 'lizard': 3, 'scissors': 4}
-
 def play(player1: str, player2: str) -> None:
     try:
         distance: int = (guess_dict[player1] - guess_dict[player2]) % 5
+        print("equal" if distance == 0 else player1 + " wins" if 0 < distance <= 2 else player2 + " wins")
     except:
-        distance: int = -1
         print("Please choose two from:\n" + "\n".join(guess_dict))
-    
-    if distance == 0: print("equal") 
-    if 0 < distance <= 2: print(player1, "wins")
-    if distance > 2: print(player2, "wins")
 
 if __name__ == "__main__":
     play(player1 = 'scissors', player2 = 'paper')
