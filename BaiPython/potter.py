96b3e1b12e2f6222714d671d1a8c7736758f237f
xiabai84@gmail.com <xiabai84@gmail.com>
2019-11-17 22:42:45 +0100

    life is short, use python

--- BaiPython/potter.py
+++ BaiPython/potter.py
@@ -1,36 +1,55 @@
 # -*- coding: utf-8 -*-
+from collections import defaultdict
 
-def find_min(x1, x2, x3, x4, x5):
+_dp_combi_value = defaultdict(lambda: 0)
+
+def find_best_price(x1: int, x2: int, x3: int, x4: int, x5: int) -> float:
     cart = [x1, x2, x3, x4, x5]
     cart.sort() # let x1>=x2>=x3>=x4>=x5
     
-    if cart[0] < 0: raise ValueError("book number can't be zero!")
+    if any(isinstance(x, int) == False for x in cart): raise ValueError("input must be integer")
+    
+    if cart[0] < 0: raise ValueError("book number can't be less than zero!")
     
     x1, x2, x3, x4, x5 = cart[4], cart[3], cart[2], cart[1], cart[0] 
     
+    if _dp_combi_value[x1, x2, x3, x4, x5] > 0: 
+        return _dp_combi_value[x1, x2, x3, x4, x5]
+    
     if x5 >= 1:
-        cost = min([
-            8 * 5 * (1 - 0.25) + find_min(x1-1, x2-1, x3-1, x4-1, x5-1),       
-            8 * 4 * (1 - 0.20) + find_min(x1-1, x2-1, x3-1, x4-1, x5),       
-            8 * 3 * (1 - 0.10) + find_min(x1-1, x2-1, x3-1, x4, x5),       
-            8 * 2 * (1 - 0.05) + find_min(x1-1, x2-1, x3, x4, x5)])
-    elif x4 >= 1:
-        cost = min([
-            8 * 4 * (1 - 0.20) + find_min(x1-1, x2-1, x3-1, x4-1, x5),       
-            8 * 3 * (1 - 0.10) + find_min(x1-1, x2-1, x3-1, x4, x5),       
-            8 * 2 * (1 - 0.05) + find_min(x1-1, x2-1, x3, x4, x5)])
-    elif x3>= 1:
-        cost = min([
-            8 * 3 * (1 - 0.10) + find_min(x1-1, x2-1, x3-1, x4, x5),       
-            8 * 2 * (1 - 0.05) + find_min(x1-1, x2-1, x3, x4, x5)])
-    elif x2 >= 1:
-        cost = min([8 * 2 * (1 - 0.05) + find_min(x1-1, x2-1, x3, x4, x5)])
-    elif x1 >= 1:
+        cost = min([8 * 5 * (1 - 0.25) + find_best_price(x1-1, x2-1, x3-1, x4-1, x5-1),       
+                    8 * 4 * (1 - 0.20) + find_best_price(x1-1, x2-1, x3-1, x4-1, x5),       
+                    8 * 3 * (1 - 0.10) + find_best_price(x1-1, x2-1, x3-1, x4, x5),       
+                    8 * 2 * (1 - 0.05) + find_best_price(x1-1, x2-1, x3, x4, x5)])
+        _dp_combi_value[x1, x2, x3, x4, x5] = cost
+        return cost
+    
+    if x4 >= 1:
+        cost = min([8 * 4 * (1 - 0.20) + find_best_price(x1-1, x2-1, x3-1, x4-1, x5),       
+                    8 * 3 * (1 - 0.10) + find_best_price(x1-1, x2-1, x3-1, x4, x5),       
+                    8 * 2 * (1 - 0.05) + find_best_price(x1-1, x2-1, x3, x4, x5)])
+        _dp_combi_value[x1, x2, x3, x4, x5] = cost
+        return cost
+    
+    if x3>= 1:
+        cost = min([8 * 3 * (1 - 0.10) + find_best_price(x1-1, x2-1, x3-1, x4, x5),       
+                    8 * 2 * (1 - 0.05) + find_best_price(x1-1, x2-1, x3, x4, x5)])
+        _dp_combi_value[x1, x2, x3, x4, x5] = cost
+        return cost
+    
+    if x2 >= 1:
+        cost = min([8 * 2 * (1 - 0.05) + find_best_price(x1-1, x2-1, x3, x4, x5)])
+        _dp_combi_value[x1, x2, x3, x4, x5] = cost        
+        return cost
+    
+    if x1 >= 1:
         cost = 8 * x1
-    else:
-        cost = 0
-    return cost
+        _dp_combi_value[x1, x2, x3, x4, x5] = cost
+        return cost
+    
+    if all (x == 0 for x in cart):
+        return 0
+
 
 if __name__ == "__main__":
-    
-    print("total cost of 2, 2, 2, 1 is ", find_min(2, 2, 2, 1, 1))
+    print("total cost is ", find_best_price(2, 2, 2, 1, 1))
