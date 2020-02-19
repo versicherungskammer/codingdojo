032e02b479e772c95a4b19d778db2c7076b339f7
Stefan Niemeyer <stefan.niemeyer@vkb.de>
2020-02-19 13:52:55 +0100

    update to Angular 9

--- StefanTypeScript/HarryPotterStefan/src/app/shared/app-material/app-material.module.ts
+++ StefanTypeScript/HarryPotterStefan/src/app/shared/app-material/app-material.module.ts
@@ -1,53 +1,54 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MatTooltipModule } from '@angular/material/tooltip';
-
-import {
-  MatToolbarModule,
-  MatIconModule,
-  MatButtonModule,
-  MatMenuModule,
-  MatCardModule,
-  MatFormFieldModule,
-  MatInputModule,
-  MatDatepickerModule,
-  MatNativeDateModule,
-  MatSelectModule,
-  MatListModule,
-  MatDialogModule,
-  MAT_DATE_LOCALE,
-  MatCheckboxModule,
-  MatAutocompleteModule,
-  MatSnackBarModule,
-  MatExpansionModule,
-  MatBadgeModule,
-  MatTableModule,
-  MatGridListModule
-} from '@angular/material';
+import { MAT_DATE_LOCALE } from '@angular/material/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+// import {
+//   MatToolbarModule,
+//   MatIconModule,
+//   MatButtonModule,
+//   MatMenuModule,
+//   MatCardModule,
+//   MatFormFieldModule,
+//   MatInputModule,
+//   MatDatepickerModule,
+//   MatNativeDateModule,
+//   MatSelectModule,
+//   MatListModule,
+//   MatDialogModule,
+//   MatCheckboxModule,
+//   MatAutocompleteModule,
+//   MatSnackBarModule,
+//   MatExpansionModule,
+//   MatBadgeModule,
+//   MatTableModule,
+//   MatGridListModule
+// } from '@angular/material';
 
 @NgModule({
   imports: [CommonModule],
   exports: [
     MatToolbarModule,
-    MatIconModule,
-    MatButtonModule,
-    MatMenuModule,
-    MatCardModule,
+    // MatIconModule,
+    // MatButtonModule,
+    // MatMenuModule,
+    // MatCardModule,
     MatFormFieldModule,
     MatInputModule,
-    MatDatepickerModule,
-    MatNativeDateModule,
-    MatSelectModule,
-    MatListModule,
-    MatDialogModule,
-    MatCheckboxModule,
-    MatAutocompleteModule,
-    MatSnackBarModule,
-    MatExpansionModule,
-    MatTooltipModule,
-    MatBadgeModule,
-    MatTableModule,
-    MatGridListModule
+    // MatDatepickerModule,
+    // MatNativeDateModule,
+    // MatSelectModule,
+    // MatListModule,
+    // MatDialogModule,
+    // MatCheckboxModule,
+    // MatAutocompleteModule,
+    // MatSnackBarModule,
+    // MatExpansionModule,
+    // MatTooltipModule,
+    // MatBadgeModule,
+    // MatTableModule,
+    // MatGridListModule
   ],
   declarations: [],
   providers: [{ provide: MAT_DATE_LOCALE, useValue: 'de-DE' }]
